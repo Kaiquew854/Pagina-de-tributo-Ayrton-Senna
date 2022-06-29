2cebf215cc75950bc7fd969e355cf8f54374431e
Kaiquew854 <80990220+Kaiquew854@users.noreply.github.com>
2022-06-28 21:57:23 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+
+function Rolar_sobre() {
+    document.querySelector('#about').scrollIntoView({
+        behavior: 'smooth'
+    });
+}
+
+function Rolar_estatisticas() {
+    document.querySelector('#Estatisticas').scrollIntoView({
+        behavior: 'smooth'
+    });
+}
+
+function Rolar_acidente() {
+    document.querySelector('#acidente').scrollIntoView({
+        behavior: 'smooth'
+    });
+}
+
+function Rolar_creditos() {
+    document.querySelector('footer').scrollIntoView({
+        behavior: 'smooth'
+    });
+}
